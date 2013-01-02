3b18d1f4295b3b5aec3be7c7b5cdb301145e1cb4
Yannick Rancillac <yannick@rancillac.fr>
2013-01-02 23:18:54 +0100

    Revert "Debut des travaux de l'agenda"
    This reverts commit 01c9f562a6ce20bf8c6156a4c98965cd9f6d4ec6.

--- src/com/examples/android/calendar/CalendarViewSampleActivity.java
+++ src/com/examples/android/calendar/CalendarViewSampleActivity.java
@@ -36,15 +36,6 @@ public class CalendarViewSampleActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        // Temp
-        //--------------------------------
-        DatePicker dp = (DatePicker)findViewById(R.id.datePicker1);
-		Intent intent = new Intent(CalendarViewSampleActivity.this , CalendarMonthViewActivity.class);
-		
-		intent.putExtra("date", dp.getYear()+"-"+dp.getMonth()+"-"+dp.getDayOfMonth());
-		startActivityForResult(intent, PICK_DATE_REQUEST);
-		//--------------------------------
-        
         Button openButton = (Button)findViewById(R.id.openButton);
         
         openButton.setOnClickListener(new OnClickListener() {
@@ -52,7 +43,7 @@ public class CalendarViewSampleActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				DatePicker dp = (DatePicker)findViewById(R.id.datePicker1);
-				Intent intent = new Intent(v.getContext(),CalendarMonthViewActivity.class);
+				Intent intent = new Intent(v.getContext(),CalendarView.class);
 				
 	    		intent.putExtra("date", dp.getYear()+"-"+dp.getMonth()+"-"+dp.getDayOfMonth());
 	    		startActivityForResult(intent, PICK_DATE_REQUEST);				
